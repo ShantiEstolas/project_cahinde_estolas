b89f016e320f3ea93dca310262cd2e44de2da70b
Rafael John Boltron Surnet <surnet_rafael@yahoo.com>
2013-02-28 15:00:57 +0800

    project 1-21

---
+++ project_cahinde_estolas/main.cpp
@@ -0,0 +1,117 @@
+#include <iostream>
+#include "Project.h"
+#include <string>
+#include <cstdlib>
+
+
+using namespace std;
+
+string problem[] {"Multiples of 3 and 5", "Even Fibonacci", "Largest Prime Factor of 600851475143", "Largest Palindrome", "Smallest Multiple", "Sum Square Difference", "10001st Prime", "Largest Product in Series","Special Pythagorean Triplet","Summation of primes","Largest Product in a Grid","Highly Divisible Triangular Number","Large Sum","Longest Collatz Sequence","Diophantine Reciprocals A","Powr Digit Sum","Number Letter Counts","Maximum Path Sum I","Counting Sundays","Factorial Digit Sum" ,"Name Scores(","Exit"};
+const int num_problems=21;
+const int Exit=num_problems+1;
+
+int main()
+{
+
+    bool done = false;
+    int i = 1, ch = 0;
+    Project proj;
+
+    while (done == false)
+    {
+        system("cls");
+        for (i = 0; i < num_problems+1; i++) {
+            cout << i+1 << ". " << problem[i] << endl;
+        }
+		cout<<"Choose problem: ";
+		cin>>ch;
+		switch (ch)
+		{
+		case 1:
+
+				proj.multiple_3and5();
+				break;
+
+            case 2:
+                proj.even_fibonacce();
+                break;
+
+            case 3:
+                proj.largest_prime_factor();
+                break;
+
+            case 4:
+                proj.largest_palindrome();
+                break;
+
+            case 5:
+                proj.smallest_multiple();
+                break;
+
+            case 6:
+                proj.sum_square_diff();
+                break;
+
+            case 7:
+                proj.ten_th_1st_prime();
+                break;
+
+            case 8:
+                proj.largest_product_in_series();
+                break;
+            case 9:
+                proj.special_pytho_triplet();
+                break;
+            case 10:
+                 proj.summation_of_primes();
+                 break;
+            case 11:
+                proj.largest_product_grid();
+                break;
+            case 12:
+                proj.divisible_triangular_number();
+                break;
+            case 13:
+                proj.largest_sum();
+                break;
+            case 14:
+                proj.longest_collatz_sequence();
+                break;
+            case 15:
+                proj.diophantine_reciprocals_a();
+                break;
+            case 16:
+                proj.power_digit_sum();
+                break;
+            case 17:
+                proj.number_letter_counts();
+                break;
+            case 18:
+                proj.max_path_sum_one();
+                break;
+            case 19:
+                proj.counting_sundays();
+                break;
+            case 20:
+                proj.factorial_digit_sum();
+                break;
+            case 21:
+                proj.name_scores();
+                break;
+
+            case Exit:
+				cout<<"\n\n       |**************************************************************|"<<endl;
+				cout<<"       |                                                              |"<<endl;
+				cout<<"       |          Thank you! Good Bye! God Bless!  SMILE  ;D          |"<<endl;
+				cout<<"       |                                                              |"<<endl;
+				cout<<"       |**************************************************************|\n\n";
+				done=true;
+				break;
+
+		}
+		system("PAUSE");
+    };
+
+
+    return 0;
+}
---
+++ project_cahinde_estolas/project.h
@@ -0,0 +1,44 @@
+#include <string>
+#ifndef PROJECT_H
+#define PROJECT_H
+
+
+class Project
+{
+	public:
+		Project();
+		virtual ~Project();
+		void multiple_3and5();//PROBLEM NO.1
+		void even_fibonacce();//PROBLEM NO.2
+		void largest_prime_factor();//PROBLEM NO.3
+		void largest_palindrome();//PROBLEM NO.4
+		void smallest_multiple();//PROBLEM NO.5
+		void sum_square_diff();//PROBLEM NO.6
+		void ten_th_1st_prime();//PROBLEM NO.7
+		void largest_product_in_series();//PROBLEM NO.8
+		void special_pytho_triplet();//PROBLEM NO.9
+		void summation_of_primes();//PROBLEMNO.10
+		void largest_product_grid();//PROBLEM NO.11
+		void divisible_triangular_number();//PROBLEM NO.12
+		void largest_sum();//PROBLEM NO.13
+		void longest_collatz_sequence();//PROBLEM NO.14
+		void diophantine_reciprocals_a();//PROBLEM NO.15
+		void power_digit_sum();//PROBLEM NO.16
+		void number_letter_counts();//PROBLEM NO.17
+		void max_path_sum_one();//PROBLEM NO.18
+		void counting_sundays();//PROBLEM NO.19
+		void factorial_digit_sum();//PROBLEM NO. 20
+		void  name_scores();//PROBLEM NO.21
+
+
+
+
+	protected:
+	private:
+		void m_diophantine_reciprocals_a(int);
+		int m_longest_collatz_sequence(int);//PROBLEM NO.14
+
+
+};
+
+#endif // PROJECT_H
